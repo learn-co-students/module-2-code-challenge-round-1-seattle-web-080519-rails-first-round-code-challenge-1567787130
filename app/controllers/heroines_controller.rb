04aefa468f65f6ccd6ac919b65a0864dbef41dea
github username <github email address>
2019-09-06 10:53:51 -0700

    Super Heroine MOD2 Rails Challenge

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,36 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+  
+  def edit
+    @heroine = Heroine.find(params[:id])
+  end
+###
+def create
+  @heroine = Heroine.new(heroine_params)
+  @heroine.save
+  # byebug
+  redirect_to heroine_path(@heroine)
+  end
+
+  def update
+    @heroine = Heroine.find(params[:id])
+    @heroine.update(heroine_params(:name))
+    redirect_to heroine_path(@heroine)
+    end
+  
+    private
+  
+    def heroine_params
+      params.require(:heroine).permit(:name, :super_name, :power_id)
+    end
+      
 end
